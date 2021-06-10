958ac8fb3013a0a347817c08ae0e0727fdd7d420
Yusuf Kaka <yusuf.kaka@mtn.com>
2021-06-10 18:52:13 +0200

    share response in chat

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-  
+    var Ayoba = getAyoba();
+
+    $("#shareButton").hide();
     var magic8Ball = {};
     magic8Ball.listofanswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
    
@@ -12,6 +14,7 @@ $(document).ready(function(){
       $("#8ball").effect( "shake" );
       $("#answer").text( answer );
       $("#answer").fadeIn(3000);
+      $("#shareButton").show();
       $("#8ball").attr("src", "img/answerside.png");
   
       console.log(question);
@@ -28,4 +31,29 @@ $(document).ready(function(){
     };
     
     $("#questionButton").click( onClick );
+    $("#shareButton").click( Ayoba.sendMessage("Question: "+ question + "\nAnswer: "+ answer) );
   });
+ 
+/**
+* Determine the mobile operating system and returns the
+* proper javascript interface
+*/
+function getAyoba() {
+   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+ 
+   // Windows Phone must come first because its UA also contains "Android"
+   if (/windows phone/i.test(userAgent)) {
+       return null;
+   }
+ 
+   if (/android/i.test(userAgent)) {
+       return "Android";
+   }
+ 
+   // iOS detection from: http://stackoverflow.com/a/9039885/177710
+   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+       return null; // todo
+   }
+ 
+   return "unknown";
+}
